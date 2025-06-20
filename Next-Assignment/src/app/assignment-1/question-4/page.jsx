7a35ffff989af3381de17c2675c3db66cb8cc3cb
Anany More <anany.more@ananymoress-475.successivetech.com>
2025-06-20 10:41:12 +0530

    Completed assignment Next-App

---
+++ Next-Assignment/src/app/assignment-1/question-4/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+import Counter from '@/component/counter'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+
+export default Page
