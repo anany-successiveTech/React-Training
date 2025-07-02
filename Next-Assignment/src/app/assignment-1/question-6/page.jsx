b4604122f1d9fb142ac3cd698833f3e8e47fa6a7
Anany More <anany.more@ananyamore-vrs-13.successivetech.com>
2025-07-02 23:46:03 +0530

    added some dependencies

--- Next-Assignment/src/app/assignment-1/question-6/page.jsx
+++ Next-Assignment/src/app/assignment-1/question-6/page.jsx
@@ -1,8 +1,9 @@
-'use client'
+"use client";
 
 import ColorButton from "@/component/ColorButton";
 import React, { useState } from "react";
-import '../../styles/colorChanger.css'
+import "../../styles/colorChanger.css";
+import Input from "@/component/Input";
 
 const App = () => {
   const [text, setText] = useState("");
@@ -12,31 +13,38 @@ const App = () => {
   const handleColorChange = (e) => setColor(e.target.value);
 
   return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <p style={{ textAlign: 'center', margin: '2rem' }}>
-        6. Create a functional component named Button that accepts props for text and color.
-        Style the button using inline styles or CSS classes based on the color prop.
-        Import and render the Button component in the App component with different text and color props.
+    <div>
+      <p className="description">
+        6. Create a functional component named Button that accepts props for
+        text and color. Style the button using inline styles or CSS classes
+        based on the color prop. Import and render the Button component in the
+        App component with different text and color props.
       </p>
+      <div className="colour">
 
-      <div style={{ marginBottom: "1rem" }}>
-        <input
-          type="text"
-          placeholder="Enter button text"
-          value={text}
-          onChange={handleTextChange}
-          className="input-field"
-        />
-        <input
-          type="text"
-          placeholder="Enter button color"
-          value={color}
-          onChange={handleColorChange}
-          className="input-field"
-        />
+      <div className="container dark">
+        <div className="input-group">
+          <Input
+            type="text"
+            placeholder="Enter button text"
+            value={text}
+            onChange={handleTextChange}
+            className="input-field"
+          />
+          <Input
+            type="text"
+            placeholder="Enter button color"
+            value={color}
+            onChange={handleColorChange}
+            className="input-field"
+          />
+        </div>
+
+        <div className="button-wrapper">
+          <ColorButton text={text} color={color} />
+        </div>
       </div>
-
-      <ColorButton text={text} color={color} />
+      </div>  
     </div>
   );
 };
