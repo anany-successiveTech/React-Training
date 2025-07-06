e77076b7e98367fb91f92b64ffabf9eaa05461c0
Anany More <anany.more@ananyamore-vrs-13.successivetech.com>
2025-07-06 12:16:22 +0530

    Refactored code

--- Next-Assignment/src/app/assignment-1/question-1/page.jsx
+++ Next-Assignment/src/app/assignment-1/question-1/page.jsx
@@ -1,16 +1,20 @@
-import Greeting from '@/component/greeting'
-import React from 'react'
-
+import React from "react";
+import "@/app/styles/home.css";
+import Greeting from "@/component/greeting";
 const Page = () => {
   return (
     <div>
-      <p style={{ textAlign: 'center', margin:'2rem'}}>
-        1. Create a functional component named Greeting that displays a simple "Hello, Next.js!" message on the screen.
-        Import and render the Greeting component in the App component.
+      <p style={{ textAlign: "center", margin: "2rem" }}>
+        1. Create a functional component named Greeting that displays a simple
+        "Hello, Next.js!" message on the screen. Import and render the Greeting
+        component in the App component.
       </p>
-      <Greeting />
+      <div className="header">
+        <h2>Welcome to the Greeting Component!</h2>
+        <Greeting/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- Next-Assignment/src/app/assignment-1/question-6/page.jsx
+++ Next-Assignment/src/app/assignment-1/question-6/page.jsx
@@ -1,10 +1,9 @@
-'use client'
+"use client";
 
 import ColorButton from "@/component/colorButton";
 import React, { useState } from "react";
 
-
-const App = () => {
+const Page = () => {
   const [text, setText] = useState("");
   const [color, setColor] = useState("");
 
@@ -13,10 +12,11 @@ const App = () => {
 
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
-         <p style={{ textAlign: 'center', margin:'2rem'}}>
-     6. Create a functional component named Button that accepts props for text and color.
-Style the button using inline styles or CSS classes based on the color prop.
-Import and render the Button component in the App component with different text and color props.
+      <p style={{ textAlign: "center", margin: "2rem" }}>
+        6. Create a functional component named Button that accepts props for
+        text and color. Style the button using inline styles or CSS classes
+        based on the color prop. Import and render the Button component in the
+        App component with different text and color props.
       </p>
       <div style={{ marginBottom: "1rem" }}>
         <input
@@ -24,20 +24,30 @@ Import and render the Button component in the App component with different text 
           placeholder="Enter button text"
           value={text}
           onChange={handleTextChange}
-          style={{ padding: "1rem", marginRight: "1rem", borderRadius:"0.5rem", border:"none"}}
+          style={{
+            padding: "1rem",
+            marginRight: "1rem",
+            borderRadius: "0.5rem",
+            border: "none",
+          }}
         />
         <input
           type="text"
           placeholder="Enter button color"
           value={color}
           onChange={handleColorChange}
-          style={{ padding: "1rem", marginRight: "1rem", borderRadius:"0.5rem", border:"none"}}
+          style={{
+            padding: "1rem",
+            marginRight: "1rem",
+            borderRadius: "0.5rem",
+            border: "none",
+          }}
         />
       </div>
 
-     <ColorButton text={text} color={color}/>
+      <ColorButton text={text} color={color} />
     </div>
   );
 };
 
-export default App;
+export default Page;
