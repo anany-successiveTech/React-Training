f8bab1561466cb6c91695ca7b5b513c659d6b24a
Anany More <anany.more@ananyamore-vrs-13.successivetech.com>
2025-07-06 17:14:24 +0530

    refactored code

--- Next-Assignment/src/component/Button.jsx
+++ Next-Assignment/src/component/Button.jsx
@@ -1,10 +1,6 @@
 
 import React from 'react';
-<<<<<<< HEAD
-import '@/app/styles/navBar.css'
-=======
 import "@/app/styles/home.css"
->>>>>>> 2477047551f0578600795f127bb09a54de0dc281
 
 const Button = ({ name, value, onclick, active}) => {
   return (
